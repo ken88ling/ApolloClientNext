e7724a7c032855aad4749ad015618a612a7f1487
ken88ling <ken88ling@gmail.com>
2021-07-12 17:49:48 +0800

    refactor code - toolbar

--- components/SpeakerListItem.js
+++ components/SpeakerListItem.js
@@ -1,76 +1,31 @@
-import { useApolloClient, useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { GET_SPEAKERS } from "../graphql/queries";
-import { ADD_SPEAKER } from "../graphql/mutations";
 import Toolbar from "./Toolbar";
 import React from "react";
 import SpeakerItem from "./SpeakerItem";
 
 export default function SpeakerListItem(props) {
   const { loading, error, data } = useQuery(GET_SPEAKERS);
-  const [addSpeaker] = useMutation(ADD_SPEAKER);
-  const apolloClient = useApolloClient();
-
   if (loading) return <div className="col-sm6">Loading...</div>;
   if (error) return <div className="col-sm6">Error</div>;
 
   return (
     <>
-      <div style={{ margin: "12px" }}>
-        <h4>Create New Speaker</h4>
-        <Toolbar
-          sortByIdDescending={() => {
-            const { speakers } = apolloClient.cache.readQuery({
-              query: GET_SPEAKERS,
-            });
-            apolloClient.cache.writeQuery({
-              query: GET_SPEAKERS,
-              data: {
-                speakers: {
-                  __typename: "SpeakerResults",
-                  datalist: [...speakers.datalist].sort((a, b) => b.id - a.id),
-                },
-              },
-            });
-          }}
-          insertSpeakerEvent={(first, last, favorite) => {
-            addSpeaker({
-              variables: {
-                first,
-                last,
-                favorite,
-              },
-              // refetchQueries: [{ query: GET_SPEAKERS }],
-              // todo let's update the cache instead re-fetch data from server again
-              update: (cache, { data: { addSpeaker } }) => {
-                const { speakers } = cache.readQuery({
-                  query: GET_SPEAKERS,
-                });
-                cache.writeQuery({
-                  query: GET_SPEAKERS,
-                  data: {
-                    speakers: {
-                      __typename: "SpeakerResults",
-                      datalist: [addSpeaker, ...speakers.datalist],
-                    },
-                  },
-                });
-              },
-            });
-          }}
-        />
-      </div>
+      <Toolbar />
       <hr />
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            {data.speakers.datalist.map(({ id, first, last, favorite, fullName }) => {
-              return (
-                <SpeakerItem
-                  key={id}
-                  speakerRec={{ id, first, last, favorite, fullName }}
-                />
-              );
-            })}
+            {data.speakers.datalist.map(
+              ({ id, first, last, favorite, fullName }) => {
+                return (
+                  <SpeakerItem
+                    key={id}
+                    speakerRec={{ id, first, last, favorite, fullName }}
+                  />
+                );
+              }
+            )}
           </div>
         </div>
       </div>
--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,16 +1,60 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { GET_SPEAKERS } from "../graphql/queries";
+import { useApolloClient, useMutation } from "@apollo/client";
+import { ADD_SPEAKER } from "../graphql/mutations";
 
-export default function Toolbar({ insertSpeakerEvent, sortByIdDescending }) {
+export default function Toolbar() {
   const { register, handleSubmit } = useForm();
+  const [addSpeaker] = useMutation(ADD_SPEAKER);
+  const apolloClient = useApolloClient();
+
   const onSubmit = (d) => {
     insertSpeakerEvent(d.first, d.last, d.favorite);
   };
 
+  const sortByIdDescending = () => {
+    const { speakers } = apolloClient.cache.readQuery({
+      query: GET_SPEAKERS,
+    });
+    apolloClient.cache.writeQuery({
+      query: GET_SPEAKERS,
+      data: {
+        speakers: {
+          __typename: "SpeakerResults",
+          datalist: [...speakers.datalist].sort((a, b) => b.id - a.id),
+        },
+      },
+    });
+  };
 
+  const insertSpeakerEvent = (first, last, favorite) => {
+    addSpeaker({
+      variables: {
+        first,
+        last,
+        favorite,
+      },
+      update: (cache, { data: { addSpeaker } }) => {
+        const { speakers } = cache.readQuery({
+          query: GET_SPEAKERS,
+        });
+        cache.writeQuery({
+          query: GET_SPEAKERS,
+          data: {
+            speakers: {
+              __typename: "SpeakerResults",
+              datalist: [addSpeaker, ...speakers.datalist],
+            },
+          },
+        });
+      },
+    });
+  };
 
   return (
     <>
+      <h4>Create New Speaker</h4>
       <form
         onSubmit={handleSubmit(onSubmit)}
         style={{ display: "flex", flexDirection: "column" }}
